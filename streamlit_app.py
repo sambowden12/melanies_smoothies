f3a820a2c13e0f10eef27e7bfad98c95ff56933f
sambowden12 <sam.bowden@ey.com>
2024-10-22 11:51:09 -0400

    Update streamlit_app.py
    added st.stop to look at dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
+import pandas as pd
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie!:cup_with_straw:")
@@ -20,6 +21,7 @@ cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 
 ingredients_list = st.multiselect(
 'Choose up to 5 ingredients:'
